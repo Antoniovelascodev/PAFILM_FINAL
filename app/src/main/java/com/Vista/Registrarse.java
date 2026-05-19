6dce10bf0c0a991aed4842663095152c6d9fc244
Pablo Ruiz <pruical2606@g.educaand.es>
2026-05-19 09:34:43 +0200

    cambios en las vistas

--- app/src/main/java/com/Vista/Registrarse.java
+++ app/src/main/java/com/Vista/Registrarse.java
@@ -10,13 +10,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.equipo.pafilm_final.R;
 
-public class IniSesion extends AppCompatActivity {
+public class Registrarse extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.pantalla_ini_sesion);
+        setContentView(R.layout.pantalla_registrarse);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
